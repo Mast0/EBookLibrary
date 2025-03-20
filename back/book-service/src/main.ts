6834becd96f61006a21e83e72aef07bea93cc9ef
Mast0 <bogdanremuga@gmail.com>
2025-03-21 00:01:08 +0200

    connect book-servise to RMQ

--- back/book-service/src/main.ts
+++ back/book-service/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { configService } from './config';
+import { Transport } from '@nestjs/microservices';
+
+const pack = require('./../package.json');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: [configService.getBrockerUri()],
+      queue: pack.name,
+      queueOptions: { durable: false }, 
+    },
+  },
+  { inheritAppConfig: true });
+
+  await app.startAllMicroservices();
+
+  await app.listen(configService.getPort());
 }
 bootstrap();
